428651256313bee8913799cf77e78a7041ddcd34
Anuj Pant <anuj.pant41@gmail.com>
2017-07-31 19:23:42 -0400

    updated index.html

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express(); // creates an instance of an express application
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+var people = {
+        person: [{name: "Bob"},{name:"Sif"},{name:"FSA"}]
+//        location: []
+//        person: {
+//            name: 'sif'
+//        },
+//        person: {
+//            name: 'anuj'
+//        }//
+    };
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true
+});
 
 app.use(morgan('combined'))
 
 app.get('/*', function (req, res) {
-  res.send('hello, world!')
+  //res.send('hello, world!')
+    res.render('index.html', {humans: people.person});
 });
 
 app.listen(3000, function () {
